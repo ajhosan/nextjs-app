ba681c35c6c88e99b18c30471d9b4d0ffc69da72
ajhosan <alexjhosan19@gmail.com>
2023-03-16 09:52:51 +0700

    productlayout

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,21 @@
 import '@/src/assets/sass/main.scss'
 import "@/src/assets/dist/styles.css";
+import { useRouter } from 'next/router';
+import ProductLayout from '@/src/components/product.layout';
+
+function AppSwitchTheme({ children }) {
+  const router = useRouter();
+
+  if (router.asPath.startsWith('/product')) {
+    return (
+      <ProductLayout>
+        {children}
+      </ProductLayout>
+    )
+  } else {
+    return children;
+  }
+}
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
