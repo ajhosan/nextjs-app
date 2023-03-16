8a647719796cdc6fa5c1058581900f86bb54e0a1
ajhosan <alexjhosan19@gmail.com>
2023-03-16 08:56:39 +0700

    edit name modal

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import { Navbar } from "@/src/components/navbar.component"
 import Button from '@/src/components/button.component';
-import Modal from '@/src/components/modal';
+import Modal from '@/src/components/modal.component';
 import { useState } from "react";
 export default function Home() {
   const [visible, setVisible] = useState(false);
